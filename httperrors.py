424fe4c6b823bc9a446c1e496e7028aeb1763899
Elizabeth Zagroba <ezagroba@mdsol.com>
2017-08-28 19:26:57 -0400

    Hard coded URL for site we're scanning

---
+++ httperrors.py
@@ -0,0 +1,35 @@
+# Mission: Find http codes that aren't in the 200 or 300 range for all the links on a single page
+
+import io
+import requests
+import string
+from bs4 import BeautifulSoup
+
+my_site = "http://www.elizabethzagroba.com"
+my_site_response = requests.get(my_site)
+page = str(BeautifulSoup(my_site_response.content, "html.parser"))
+file = io.FileIO("list_of_all_links.txt", "w")
+
+def getURL(page):
+    start_link = page.find("a href")
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"http', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1: end_quote]
+    return url, end_quote
+
+count = 0
+while True:
+    file = open('list_of_all_links.txt', 'a')
+    url, n = getURL(page)
+    page = page[n:]
+    if url:
+    	http_response = str(requests.head(url))
+    	if ("<Response [2" not in http_response) and ("<Response [3" not in http_response):
+    		file.write(http_response + url + '\n')
+    		count += 1
+    else:
+    	file.close()
+    	print("There are " + str(count) + " links without 200 or 300 range http responses on your site.")
+    	break
