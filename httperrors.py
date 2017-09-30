ffaf5a574fc9595c4a7478628b700dcf9deb2caa
Elizabeth Zagroba <ezagroba@mdsol.com>
2017-09-30 10:51:46 -0400

    [#2] Switch string parsing to status codes
    I used urllib to go with python3. I still need to do #6.

--- httperrors.py
+++ httperrors.py
@@ -1,34 +1,42 @@
 # Mission: Find http codes that aren't in the 200 or 300 range for all the links on a single page
 
 import io
+import http
 import requests
+import ssl
 import string
+import urllib.request
 from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+
 
 MY_SITE = "http://www.elizabethzagroba.com"
 my_site_response = requests.get(MY_SITE)
-page = str(BeautifulSoup(my_site_response.content, "html.parser"))
-file = io.FileIO("list_of_all_links.txt", "w")
+only_external_links = SoupStrainer(target="_blank")
+page = str(BeautifulSoup(my_site_response.content, "html.parser", parse_only=only_external_links))
+file = io.FileIO('list_of_all_links.txt', 'w')
+ssl._create_default_https_context = ssl._create_unverified_context # allows opening of links on page w/o ssl errors
 
 def getURL(page):
-    start_link = page.find("a href")
-    if start_link == -1:
-        return None, 0
-    start_quote = page.find('"http', start_link)
-    end_quote = page.find('"', start_quote + 1)
-    url = page[start_quote + 1: end_quote]
-    return url, end_quote
+	start_link = page.find("a href")
+	if start_link == -1:
+		return None, 0
+	start_quote = page.find('"http', start_link)
+	end_quote = page.find('"', start_quote + 1)
+	url = page[start_quote + 1: end_quote]
+	return url, end_quote
 
 count = 0
 with open('list_of_all_links.txt', 'a') as file:
-    while True: 
-	    url, n = getURL(page)
-	    page = page[n:]
-	    if url:
-	    	http_response = str(requests.head(url))
-	    	if ("<Response [2" not in http_response) and ("<Response [3" not in http_response):
-	    		file.write(http_response + url + '\n')
-	    		count += 1
-	    else:
-	    	print("There are " + str(count) + " links without 200 or 300 range http responses on your site.")
-	    	break
+	while True: 
+		url, n = getURL(page)
+		page = page[n:]
+		if url:
+			try:
+				req = urllib.request.urlopen(url)
+			except urllib.error.URLError as explanation:
+				file.write(str(explanation) + " " + url + '\n')
+				count += 1
+		else:
+			print("There are " + str(count) + " links outside of the 200 or 300 range of http responses on your site.")
+			break
