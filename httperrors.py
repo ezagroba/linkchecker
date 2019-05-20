0024a35a00339143bc214ff085f968324a36dc2f
Elizabeth Zagroba <1462795+ezagroba@users.noreply.github.com>
2019-05-20 21:56:22 +0200

    PEP8 fixes
    PEP8 fixes that were underlined when I opened it in PyCharm, now fixed.

--- httperrors.py
+++ httperrors.py
@@ -16,24 +16,26 @@ my_site_response = requests.get(MY_SITE)
 only_external_links = SoupStrainer(target="_blank")
 page = str(BeautifulSoup(my_site_response.content, "html.parser", parse_only=only_external_links))
 
-def getURL(page):
-	start_link = page.find("a href")
+
+def get_url(website):
+	start_link = website.find("a href")
 	if start_link == -1:
 		return None, 0
-	start_quote = page.find('"http', start_link)
-	end_quote = page.find('"', start_quote + 1)
-	url = page[start_quote + 1: end_quote]
-	return url, end_quote
+	start_quote = website.find('"http', start_link)
+	end_quote = website.find('"', start_quote + 1)
+	stripped_url = website[start_quote + 1: end_quote]
+	return stripped_url, end_quote
+
 
 try:
-    os.remove('list_of_all_links.txt')
+	os.remove('list_of_all_links.txt')
 except OSError:
-    pass
+	pass
 
 count = 0
 with open('list_of_all_links.txt', 'a') as file:
 	while True: 
-		url, n = getURL(page)
+		url, n = get_url(page)
 		page = page[n:]
 		if url:
 			try:
